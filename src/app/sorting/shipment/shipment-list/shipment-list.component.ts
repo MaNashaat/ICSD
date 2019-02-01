0dfcfdf7117a558292505cbaadc1f22cb36f2353
Ahmed Elhady <Ahmed.Elhady@linkegypt.com>
2019-02-01 23:56:45 +0200

    Hady_1_2_2019

--- src/app/sorting/shipment/shipment-list/shipment-list.component.ts
+++ src/app/sorting/shipment/shipment-list/shipment-list.component.ts
@@ -1,16 +1,35 @@
-import { Component, ViewChild, OnInit, AfterViewInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
 import { PatternsService } from 'src/app/shared/services/patterns.service';
 import { Subject } from 'rxjs';
 import { DataTableDirective } from 'angular-datatables';
 import { Shipment } from '../Shipment';
 import { ShipmentService } from '../shipment.service';
-
+import { environment } from '../../../../environments/environment';
+import { FileUploader } from '../../../../../node_modules/ng2-file-upload';
+import { ActivatedRoute, Router } from '../../../../../node_modules/@angular/router';
+import { HttpClient } from '../../../../../node_modules/@angular/common/http';
+const URL = environment.apiURL + 'Shipment/Upload';
 @Component({
   selector: 'app-shipment-list',
   templateUrl: './shipment-list.component.html',
   styleUrls: ['./shipment-list.component.scss']
 })
 export class ShipmentListComponent implements OnInit, AfterViewInit {
+
+  constructor(private el: ElementRef, private _Service: ShipmentService, private route: ActivatedRoute, private router: Router, private http: HttpClient) {
+
+
+    this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+
+      this._Service.getAll().subscribe(list => {
+        this.list = list;
+        this.refreshDataSource(this.list);
+      });
+    };
+  }
+  public uploader: FileUploader = new FileUploader({ url: URL, method: 'POST' });
+  public hasBaseDropZoneOver = false;
+  public hasAnotherDropZoneOver = false;
   @ViewChild(DataTableDirective)
   public dtElement: DataTableDirective;
   dtTrigger: Subject<any> = new Subject();
@@ -19,7 +38,15 @@ export class ShipmentListComponent implements OnInit, AfterViewInit {
   @Output() notify: EventEmitter<Shipment> = new EventEmitter<Shipment>();
 
   dtOptions: DataTables.Settings = {};
-  constructor(private patterns: PatternsService, private _service: ShipmentService) {}
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e: any): void {
+    this.hasAnotherDropZoneOver = e;
+  }
+
+
 
   ngAfterViewInit(): void {
     this.dtTrigger.next();
@@ -32,9 +59,9 @@ export class ShipmentListComponent implements OnInit, AfterViewInit {
     this.notify.emit(itm);
   }
 
-  delete = function(id: number) {
+  delete = function (id: number) {
     this._service.remove(id).subscribe(result => {
-      const filterResult = this.list.filter(function(element, index, array) {
+      const filterResult = this.list.filter(function (element, index, array) {
         return element.ID === id;
       });
       const index: number = this.list.indexOf(filterResult[0]);
@@ -48,6 +75,10 @@ export class ShipmentListComponent implements OnInit, AfterViewInit {
       stateSave: true
     };
     this.refreshDataSource(this.list);
+    this.uploader.onBeforeUploadItem = (item) => {
+      item.withCredentials = false;
+    };
+    // this.uploader.onaf;
   }
 
   refreshDataSource(list): void {
@@ -59,4 +90,6 @@ export class ShipmentListComponent implements OnInit, AfterViewInit {
       });
     }
   }
+
+
 }
